b9e3283258f70dc816728b5191430d545c67f3fc
Esha Eman <109205096+EshaEman11@users.noreply.github.com>
2023-11-19 22:09:35 +0500

    Fetch post in Home page

--- insta-clone/src/components/Home.js
+++ insta-clone/src/components/Home.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import './Home.css';
 import { useNavigate } from "react-router-dom";
 
 export default function Home() {
     const navigate = useNavigate();
+    const [data, setData] = useState([]) 
 
     useEffect(() => {
 
@@ -11,21 +12,34 @@ export default function Home() {
         if (!token) {
             navigate("./signup")
         }
+
+        //fetching all posts
+        fetch("http://localhost:5000/allposts", {
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem("jwt")
+            }
+        }).then(res => res.json())
+            .then(result => setData(result))
+            .catch(err => console.log(err))
+
+
     }, [])
     return (
         <div className="home">
             {/* card */}
-            <div className="card">
+            {data.map((posts)=>{
+                return(
+                    <div className="card">
                 {/*card header*/}
                 <div className="card-header">
                     <div className="card-pic">
                         <img src="https://images.unsplash.com/photo-1621342261924-3e2f6c9603f5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c3F1YXJlJTIwaW1hZ2V8ZW58MHx8MHx8fDA%3D" alt="" />
                     </div>
-                    <h5> Esha</h5>
+                    <h5> {posts.postedBy.name}</h5>
                 </div>
                 {/*card image*/}
                 <div className="card-image">
-                    <img src="https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTV8fHRlY2hub2xvZ3l8ZW58MHx8MHx8fDA%3D" alt="" />
+                    <img src= {posts.photo} alt="" />
                 </div>
                 {/*card content*/}
                 <div className="card-content">
@@ -33,7 +47,7 @@ export default function Home() {
                         favorite
                     </span>
                     <p>1 Like</p>
-                    <p>This is amazing</p>
+                    <p>{posts.body}</p>
                 </div>
                 {/*add comment*/}
                 <div className="add-comment">
@@ -45,6 +59,10 @@ export default function Home() {
                 </div>
 
             </div>
+
+                )
+            })}
+            
         </div>
     )
 }
